ec12bad3494c735242f0d48bc876ffb15d8a4b57
reeba <iamneeba@gmail.com>
2026-04-03 14:28:31 +0500

    test: add initial unit tests for user authentication

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from apps.accounts.models import User
 
-# Create your tests here.
+class UserAuthTest(TestCase):
+
+    def setUp(self):
+        # Create users with different roles
+        self.owner = User.objects.create_user(
+            username='owner',
+            password='test123',
+            Role='OWNER'
+        )
+
+        self.staff = User.objects.create_user(
+            username='staff',
+            password='test123',
+            Role='STAFF'
+        )
+
+        self.customer = User.objects.create_user(
+            username='customer',
+            password='test999',
+            Role='CUSTOMER'
+        )
+
+    def test_owner_can_login(self):
+        login = self.client.login(username='owner', password='test123')
+        self.assertTrue(login)
+
+    def test_staff_can_login(self):
+        login = self.client.login(username='staff', password='test123')
+        self.assertTrue(login)
+
+    def test_customer_can_login(self):
+        login = self.client.login(username='customer', password='test999')
+        self.assertTrue(login)
