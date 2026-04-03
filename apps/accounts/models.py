18a7ffb170b1fca58f5bc58da30cc0c885e2277f
reeba <iamneeba@gmail.com>
2026-04-03 14:27:53 +0500

    feat: Create accounts app with custom User model and roles

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    class Role(models.TextChoices): 
+        OWNER = 'OWNER',    'Owner' # full access
+        STAFF = 'STAFF',    'Staff' # operational access
+        CUSTOMER = 'CUSTOMER',    'Customer' # portal access only
+
+    Role = models.CharField(
+        max_length=10,
+        choices=Role.choices,
+        default=Role.STAFF
+    )
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+
