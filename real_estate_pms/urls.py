402834f8ac4962d1edc721983c922538d9864d3a
reeba <iamneeba@gmail.com>
2026-04-09 15:20:25 +0500

    feat(projects): add project list view and configure URL routing

--- real_estate_pms/urls.py
+++ real_estate_pms/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('apps.accounts.urls')), # this connects your view
-    path('projects/', include('apps.projects.urls')),  
+    path('accounts/', include('apps.accounts.urls')), 
+    path('projects/', include('apps.projects.urls', namespace='projects')),
+    path('plots/',include('apps.plots.urls')),
 ]
