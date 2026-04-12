f449ae3097a184d9cf378d36cf5a9dcb116b6c4e
reeba <iamneeba@gmail.com>
2026-04-12 16:03:02 +0500

    chore(settings): update base configuration for custom user model and auth settings

--- real_estate_pms/settings/base.py
+++ real_estate_pms/settings/base.py
@@ -11,12 +11,19 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-MEDIA_URL  = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -43,6 +50,7 @@ INSTALLED_APPS = [
     'apps.accounts',
     'apps.projects',
     'apps.plots',
+    'apps.customer',
 ]
 
 MIDDLEWARE = [
